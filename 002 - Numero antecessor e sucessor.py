6059547e2de6f9f4c12941c0a70c67ffd9b17956
Carlos-Henrique98 <ichfs9108@gmail.com>
2019-12-17 14:29:20 -0300

    002 - antecessor e sucessor

---
+++ 002 - Numero antecessor e sucessor.py
@@ -0,0 +1,7 @@
+'002 - Faça um programa que leia um número inteiro'
+'e mostre na tela o seu sucessor e seu antecessor'
+
+n1 = int(input("Informe um número:"))
+print("Número digitado:",n1)
+print("Antecessor:", n1 - 1)
+print("Sucessor:", n1 + 1)
